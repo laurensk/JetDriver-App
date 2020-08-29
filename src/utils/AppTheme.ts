e71bb1dc96b629dc1c72a7f9ebcfb1ed5552a413
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-08-29 22:44:02 +0200

    app theme textcolor

--- src/utils/AppTheme.ts
+++ src/utils/AppTheme.ts
@@ -3,10 +3,12 @@ import {useColorScheme} from 'react-native';
 
 const DefaultTheme: {[k: string]: string} = {
   backgroundColor: '#ffffff',
+  textColor: '#000000',
 };
 
 const DarkTheme: {[k: string]: string} = {
   backgroundColor: '#000000',
+  textColor: '#ffffff',
 };
 
 export const DefaultThemeContext = React.createContext(DefaultTheme);
