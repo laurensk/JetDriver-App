ee180b42bd9aa06cdba725b8d16cd7c93c761195
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-08-30 21:25:41 +0200

    entry model

---
+++ src/models/Entry.ts
@@ -0,0 +1,14 @@
+interface Entry {
+  uuid: string;
+  date: Date;
+  startMileage: number;
+  endMileage: number;
+  routeDest: string;
+  notes: string;
+  car: null;
+  roadCondition: null;
+  companion: null;
+  user: null;
+}
+
+export default Entry;
