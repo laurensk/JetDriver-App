c379133a3cb0f31df062b4faa72ec7b3dd46af9c
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-09-08 12:52:27 +0200

    added name to user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,12 @@
 export class User {
   uuid: string;
+  name: string;
   email: string;
   token: string;
 
-  constructor(uuid: string, email: string, token: string) {
+  constructor(uuid: string, name: string, email: string, token: string) {
     this.uuid = uuid;
+    this.name = name;
     this.email = email;
     this.token = token;
   }
