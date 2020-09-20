29de780b87f11274240e459f912d7457e2ada84e
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-09-20 19:48:08 +0200

    improved back swipe

--- src/toolbox/ScrollViewBackView.tsx
+++ src/toolbox/ScrollViewBackView.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import {View} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
 
 export default (props: any) => {
   return (
     <View style={{flex: 1}}>
-      <ScrollView>{props.children}</ScrollView>
+      {props.children}
       <View
         style={{
           position: 'absolute',
