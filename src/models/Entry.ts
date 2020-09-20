d4e6861ca05030686e7fbf4fc592e2642dd788c5
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-09-20 14:54:17 +0200

    added daytime with hotfix

--- src/models/Entry.ts
+++ src/models/Entry.ts
@@ -2,39 +2,46 @@ import {User} from './User';
 import {Companion} from './Companion';
 import {RoadCondition} from './RoadCondition';
 import {Car} from './Car';
+import {Daytime} from './Daytime';
 
 export class Entry {
   uuid: string;
-  date: Date;
+  startDate: number;
+  endDate: number;
   startMileage: number;
   endMileage: number;
   routeDest: string;
   notes: string;
   car: Car;
   roadCondition: RoadCondition;
+  daytimeId: Daytime;
   companion: Companion;
   user: User;
 
   constructor(
     uuid: string,
-    date: Date,
+    startDate: number,
+    endDate: number,
     startMileage: number,
     endMileage: number,
     routeDest: string,
     notes: string,
     car: Car,
     roadCondition: RoadCondition,
+    daytimeId: Daytime,
     companion: Companion,
     user: User
   ) {
     this.uuid = uuid;
-    this.date = date;
+    this.startDate = startDate;
+    this.endDate = endDate;
     this.startMileage = startMileage;
     this.endMileage = endMileage;
     this.routeDest = routeDest;
     this.notes = notes;
     this.car = car;
     this.roadCondition = roadCondition;
+    this.daytimeId = daytimeId;
     this.companion = companion;
     this.user = user;
   }
